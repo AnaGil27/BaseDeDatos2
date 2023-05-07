6fd43338b58c65f8a3ebff1ef9b478395577f4c9
AnaGil27 <gilyalta27@gmail.com>
2023-05-07 10:45:48 -0500

    initial commit

---
+++ src/main/java/pe/edu/cibertec/BaseDeDatos2/BaseDeDatos2Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.BaseDeDatos2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseDeDatos2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseDeDatos2Application.class, args);
+	}
+
+}
